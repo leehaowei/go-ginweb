6aa0065ee08cf1bb0dbf4391a1436f6b1416127e
Haowei Lee <haoweilee.tpe@gmail.com>
2022-06-07 23:01:30 +0200

    add PostForm

--- main.go
+++ main.go
@@ -20,42 +20,13 @@ func main() {
 
 	router.GET("/", index) // handler for root
 
-	router.GET("/users", func(c *gin.Context) {
+	router.POST("/login", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"userId": c.Query("id"),
-			"sex":    c.Query("sex"),
-			"age":    c.DefaultQuery("age", "18"),
+			"uid":        c.PostForm("uid"),
+			"pwd":        c.PostForm("pwd"),
+			"rememberMe": c.DefaultPostForm("rememberMe", "0"),
 		})
 	})
-
-	//router.GET("/users/:id", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"userId": c.Param("id"),
-	//	})
-	//})
-	//router.GET("news/:id", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"newsID": c.Param("id"),
-	//	})
-	//})
-
-	// define router Group - V1 (version1)
-	//var apiV1 = router.Group("/v1")
-	//apiV1.GET("/user", func(c *gin.Context) {
-	//	c.String(http.StatusOK, "User GET service v1")
-	//})
-	//apiV1.GET("news", func(c *gin.Context) {
-	//	c.String(http.StatusOK, "NEWS GET service v1")
-	//})
-
-	// define router Group - V2 (version2)
-	//var apiV2 = router.Group("/v2")
-	//apiV2.GET("/user", func(c *gin.Context) {
-	//	c.String(http.StatusOK, "User GET service v2")
-	//})
-	//apiV2.GET("/news", func(c *gin.Context) {
-	//	c.String(http.StatusOK, "NEWS GET service v2")
-	//})
 
 	router.Run(port)
 }
