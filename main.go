a841897fff85ba04734855dc917420f580030e58
Haowei Lee <haoweilee.tpe@gmail.com>
2022-06-10 00:40:05 +0200

    add self defined validation

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func index(c *gin.Context) {
@@ -11,9 +15,22 @@ func index(c *gin.Context) {
 	})
 }
 
-type uriNewsBinding struct {
-	Region   string `json:"region" binding:"required"`
-	Category string `json:"category" binding:"required"`
+type User struct {
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required,password"`
+	Email    string `json:"email" binding:"required,email"`
+	Role     string `json:"role" binding:"required,oneof=ADMIN USER"`
+}
+
+func verifyPassword(field validator.FieldLevel) bool {
+	var regex = regexp.MustCompile("\\w{8,}")
+	var password = field.Field().String()
+	if regex.MatchString(password) {
+		if strings.Index(password, "!") > -1 {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -23,19 +40,24 @@ func main() {
 	var router = gin.Default()
 	var port = ":8080"
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("password", verifyPassword)
+	}
+
 	router.GET("/", index) // handler for root
 
-	router.POST("/news", func(c *gin.Context) {
-		var binding uriNewsBinding
-
-		if err := c.ShouldBindJSON(&binding); err != nil {
+	router.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
 			c.String(http.StatusBadRequest, err.Error())
 			return
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"region":   binding.Region,
-			"category": binding.Category,
+			"name":     user.Name,
+			"password": user.Password,
+			"email":    user.Email,
+			"role":     user.Role,
 		})
 	})
 
