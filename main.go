8fcd793a8e7847e914f615a2d989fedc4b40ad86
Haowei Lee <haoweilee.tpe@gmail.com>
2022-06-10 08:53:35 +0200

    add html template

--- main.go
+++ main.go
@@ -2,64 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 	"net/http"
-	"regexp"
-	"strings"
 )
 
-func index(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello go gin web.",
-	})
-}
-
-type User struct {
-	Name     string `json:"name" binding:"required"`
-	Password string `json:"password" binding:"required,password"`
-	Email    string `json:"email" binding:"required,email"`
-	Role     string `json:"role" binding:"required,oneof=ADMIN USER"`
-}
-
-func verifyPassword(field validator.FieldLevel) bool {
-	var regex = regexp.MustCompile("\\w{8,}")
-	var password = field.Field().String()
-	if regex.MatchString(password) {
-		if strings.Index(password, "!") > -1 {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
-	//gin.SetMode(gin.ReleaseMode)
-	//gin.SetMode(gin.DebugMode)
 
 	var router = gin.Default()
 	var port = ":8080"
 
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("password", verifyPassword)
-	}
+	router.LoadHTMLGlob("./templates/*")
 
-	router.GET("/", index) // handler for root
-
-	router.POST("/users", func(c *gin.Context) {
-		var user User
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.String(http.StatusBadRequest, err.Error())
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"name":     user.Name,
-			"password": user.Password,
-			"email":    user.Email,
-			"role":     user.Role,
+	router.GET("/profile", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "profile.html", gin.H{
+			"name": "frankfurt",
+			"role": "ADMIN",
+			"age":  25,
 		})
 	})
+
+	router.StaticFile("/", "./static/index.html")
 
 	router.Run(port)
 }
